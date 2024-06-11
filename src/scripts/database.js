7a3b12df65ab8b43793a09812ffb79fe44c3d1bc
christian_santana <crsantanarj@hotmail.com>
2024-06-11 17:58:16 -0300

    feat: Estruturando o projeto, e populando a pasta assets e o arquivo database.js

---
+++ src/scripts/database.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        id: 1,
+        name: "Cadeira",
+        price: 238.41,
+        discount: 15,
+        image: "src/assets/images/chair.png",
+        description:
+          "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    },
+    {
+        id: 2,
+        name: "Relógio",
+        price: 238.41,
+        discount: 25,
+        image: "src/assets/images/clock.png",
+        description:
+          "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    },
+    {
+        id: 3,
+        name: "Luminária",
+        price: 238.41,
+        discount: 20,
+        image: "src/assets/images/shovel.png",
+        description:
+          "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    },
+    {
+        id: 4,
+        name: "Poltrona",
+        price: 238.41,
+        discount: 30,
+        image: "src/assets/images/armchair.png",
+        description:
+          "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    },
+]
